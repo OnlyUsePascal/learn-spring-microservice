ddde1c06da853e08da870febacb701284c1bda05
joun <chuyengianhorang@gmail.com>
2025-10-10 11:02:03 +0700

    feat: api gateway

--- servicea/src/main/java/com/example/servicea/ServiceaApplication.java
+++ servicea/src/main/java/com/example/servicea/ServiceaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServiceaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceaApplication.class, args);
 	}
