84a0cea372e80cfd3d7fc4e1b3b5adb69d300602
joun <chuyengianhorang@gmail.com>
2025-10-10 16:53:55 +0700

    feat: route logging

---
+++ api-gateway/src/main/java/com/example/api_gateway/LoggingFilter.java
@@ -0,0 +1,46 @@
+package com.example.api_gateway;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class LoggingFilter implements GlobalFilter, Ordered {
+  private static final Logger log = LoggerFactory.getLogger(LoggingFilter.class);
+
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+    log.info("--> Request: {} {}",
+        exchange.getRequest().getMethod(),
+        exchange.getRequest().getURI());
+
+    Route route = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR);
+    if (route != null) {
+      System.out.println("Request matched route: " + route.getId());
+    } else {
+      System.out.println("Request did not match any route.");
+    }
+
+    return chain
+        .filter(exchange)
+        .then(Mono.fromRunnable(() -> log.info("<-- Response: {}",
+            exchange.getResponse().getStatusCode())));
+  }
+
+  @Override
+  public int getOrder() {
+    return -1;
+  }
+}
