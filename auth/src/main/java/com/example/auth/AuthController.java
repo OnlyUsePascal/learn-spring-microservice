03306a116c742d4a785cf476e8ffc4aca9d390d1
joun <chuyengianhorang@gmail.com>
2025-10-19 17:05:54 +0700

    sign up

--- auth/src/main/java/com/example/auth/AuthController.java
+++ auth/src/main/java/com/example/auth/AuthController.java
@@ -2,9 +2,16 @@ package com.example.auth;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.auth.user.CreateUserDto;
 import com.example.auth.user.UserClient;
 
+import feign.Response;
+
+import java.net.http.HttpResponse.ResponseInfo;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -13,18 +20,34 @@ public class AuthController {
   @Autowired
   UserClient userClient;
 
-  @PostMapping("/login")
-  public String login(@RequestBody LoginPayload payload) {
+  @PostMapping("/sign-in")
+  public ResponseEntity<String> signIn(@RequestBody SignInDto dto) {
     var status = "";
 
     try {
-      var u = userClient.findUserWithUsername(payload.getUsername()).getBody();
+      var u = userClient.findUserWithUsername(dto.getUsername()).getBody();
       status = "locked in! user email is: " + u.getEmail();
     } catch (Exception e) {
       status = "nah bro";
     }
 
-    return status;
+    return ResponseEntity.ok(status);
+  }
+
+  @PostMapping("/sign-up")
+  public ResponseEntity<String> signUp(@RequestBody SignUpDto dto) {
+    try {
+      var uid = userClient.createUser(CreateUserDto
+          .builder()
+          .username(dto.getUsername())
+          .password(dto.getPassword())
+          .email(dto.getEmail())
+          .build())
+          .getBody();
+      return ResponseEntity.ok(uid.toString());
+    } catch (Exception e) {
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
 }
--- auth/src/main/java/com/example/auth/SignInDto.java
+++ auth/src/main/java/com/example/auth/SignInDto.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class LoginPayload {
+public class SignInDto {
   private String username;
   private String password;
 }
