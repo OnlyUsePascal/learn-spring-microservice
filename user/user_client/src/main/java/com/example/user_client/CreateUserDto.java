e958d49f412134010c0ce62bf03c09be51806845
joun <chuyengianhorang@gmail.com>
2025-10-31 19:15:28 +0700

    add submodule user

--- user/user_client/src/main/java/com/example/user_client/CreateUserDto.java
+++
@@ -1,4 +0,0 @@
-package com.example.user_client;
-
-public record CreateUserDto(String username, String password, String email) {
-}
--- user/user_client/src/main/java/com/example/user_client/GetUserDto.java
+++
@@ -1,6 +0,0 @@
-package com.example.user_client;
-
-import java.util.UUID;
-
-public record GetUserDto(UUID id, String username, String email) {
-};
